eef8d348bb9fad6588a0669cceb8568a5cbf2d10
daniel <102396163+escdaniel@users.noreply.github.com>
2024-05-29 12:00:36 -0500

    alvrg

---
+++ src/main/java/com/riwi/simulacro_java/SimulacroJavaApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.simulacro_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacroJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulacroJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/simulacro_java/SimulacroJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.simulacro_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulacroJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
